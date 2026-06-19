8c38fcf8838eb27fb344d41fb8be57d12b8f1ded
Ricky Ryklief <rykliefr@gmail.com>
2026-06-19 19:26:52 +0100

    Feat: Move repo and create Sanity studio

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,5 +3,9 @@
 interface ImportMetaEnv {
   readonly PUBLIC_SANITY_PROJECT_ID: string;
   readonly PUBLIC_SANITY_DATASET: string;
-  readonly SANITY_API_READ_TOKEN?: string;
+  readonly WEB3FORMS_ACCESS_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
